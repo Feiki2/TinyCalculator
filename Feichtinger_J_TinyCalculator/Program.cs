0a4e03cb5f1759dce4bead5a8c1cf800ea381a2e
Feichtinger Jonas <J.Feichtinger@htlvb.at>
2026-03-20 09:09:51 +0100

    added colors

--- Feichtinger_J_TinyCalculator/Program.cs
+++ Feichtinger_J_TinyCalculator/Program.cs
@@ -1,21 +1,36 @@
 ﻿Console.WriteLine("== Tiny calculator ==");
 
+Console.ForegroundColor = ConsoleColor.Green;
+
 Console.Write("1. Operand: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("2. Operand: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine($"{a} + {b} = {a + b}");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"{a} - {b} = {a - b}");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine($"{a} * {b} = {a * b}");
-Console.WriteLine($"{a} / {b} = {a / b}");
+Console.ResetColor();
 
 if (b == 0)
 {
+    Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine($"{a} / {b} = undefiniert/unendlich");
+    Console.ResetColor();
 }
 else
 {
+    Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine($"{a} / {b} = {a / b}");
+    Console.ResetColor();
 }
